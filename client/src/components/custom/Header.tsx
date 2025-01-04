e2d2adb82ca87e62e2eefdf219c271ee051771d0
Harsh Thakur <harshprojects2002@outlook.com>
2025-01-04 18:19:41 +0530

    Changes : : Home Page
    - Changed @devharshthakur to a Github button
    - Changed about me into a button
    - Changed behaviour of theme toggle button

--- client/src/components/custom/Header.tsx
+++ client/src/components/custom/Header.tsx
@@ -1,26 +1,27 @@
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
-import { SunIcon } from '@/components/icons/icons';
 import { ModeToggle } from '../util/ThemeToggle';
+import { FaGithub } from 'react-icons/fa';
 
 export function Header() {
    return (
       <header className="flex items-center justify-between px-4 py-3 sm:px-6 md:px-8">
-         <Link
-            href="https://github.com/devharshthakur"
-            className="text-sm font-medium hover:underline"
-            prefetch={false}
-         >
-            @devharshthakur
-         </Link>
-         <div className="flex items-center gap-4">
-            <Link href="/about" className="text-sm font-medium hover:underline" prefetch={false}>
-               About Me
+         <Button variant="outline" asChild>
+            <Link href="https://github.com/devharshthakur" className="flex items-center gap-2" prefetch={false}>
+               <FaGithub className="h-5 w-5" />
+               @devharshthakur
             </Link>
+         </Button>
+         <div className="flex items-center gap-4">
+            <Button variant={'outline'} asChild>
+               <Link href="/about" className="text-sm font-medium hover:underline" prefetch={false}>
+                  About Me
+               </Link>
+            </Button>
             <Button variant={'outline'}>
                <Link href="https://www.devharshthakur.blog">Blogs</Link>
             </Button>
-            <ModeToggle className="h-5 w-5" />
+            <ModeToggle className="h-9 w-9" />
          </div>
       </header>
    );
